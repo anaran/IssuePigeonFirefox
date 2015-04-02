b60beb00dade8853eabc52ffcfec11a69a3a46b8
Adrian Aichner <adrian.aichner@gmail.com>
2015-04-03 01:56:48 +0200

    dirty commit with touchmove working in nightly firefox for android.
    TODO: change movemove to match.
    Remove move element.

--- IssuePigeon/data/extendKnownSites.js
+++ IssuePigeon/data/extendKnownSites.js
@@ -6,6 +6,7 @@
 //
 // Replace /\b(const|let)\B/ with "$1 "
 // Replace [/^( *)function (\w+)/] with [$1var $2 = function]
+// Replace [/\Bof\s*/] With [ of ]
 //
 // Author: adrian.aichner@gmail.com
 //
@@ -15,7 +16,7 @@
 (function() {
   try {
     // var exports = exports || {};
-    let DEBUG_ADDON = true;
+    let DEBUG_ADDON = false;
     //
     // NOTE Set "DEBUG_ADDON = true" in the debugger before continuing to get console messages logged.
     // Make sure option "Console Logging Level" is not set to "off".
@@ -51,6 +52,31 @@
       cancel.addEventListener('click', function (event) {
         document.body.removeChild(div);
       });
+      let move = document.createElement('span');
+      move.style.align = 'right';
+      move.innerHTML = "Move &nesear;";
+      move.addEventListener('mousemove', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        if (event.buttons == 1/* && event.currentTarget === move*/) {
+          div.style.top = (event.clientY - event.target.offsetTop - event.target.offsetHeight / 2) + 'px';
+          div.style.left = (event.clientX - event.target.offsetLeft - event.target.offsetWidth / 2) + 'px';
+        }
+      });
+      // move.addEventListener('touchstart', function (event) {
+      //   // event.preventDefault();
+      //   event.stopPropagation();
+      // });
+      ta.addEventListener('touchmove', function (e) {
+        var touchY = e.touches[e.touches.length - 1].clientY;
+        var touchX = e.touches[e.touches.length - 1].clientX;
+        if ((touchY - div.offsetTop) < div.offsetHeight * 0.9 || (touchX - div.offsetLeft) < div.offsetWidth * 0.9) {
+          e.stopPropagation();
+          // e.preventDefault();
+          div.style.left = (touchX - (((touchX - div.offsetLeft) > div.offsetWidth * 0.5) ? div.offsetWidth * 0.1 : div.offsetWidth * 0.9)) + 'px';
+          div.style.top = (touchY - (((touchY - div.offsetTop) > div.offsetHeight * 0.5) ? div.offsetHeight * 0.1 : div.offsetHeight * 0.9)) + 'px';
+        }
+      });
       div.style = 'top: 40%; left: 20%; position: fixed;';
       // Cannot have both resize and  width: 50%; height: 50%;
       ta.style = 'resize: both;';
@@ -65,28 +91,11 @@
       //       }
       buttonDiv.appendChild(save);
       buttonDiv.appendChild(cancel);
+      // buttonDiv.appendChild(move);
       div.appendChild(buttonDiv);
       div.appendChild(ta);
-  ta.addEventListener('mousemove', function (e) {
-    if (e.buttons == 1) {
-      // e.preventDefault();
-      e.stopPropagation();
-      div.style.left = (e.clientX - (((e.clientX - div.offsetLeft) > div.offsetWidth * 0.5) ? div.offsetWidth * 1 : div.offsetWidth * 0) - ta.offsetLeft) + 'px';
-      div.style.top = (e.clientY - (((e.clientY - div.offsetTop) > div.offsetHeight * 0.5) ? div.offsetHeight * 1 : div.offsetHeight * 0) - ta.offsetTop) + 'px';
-    }
-  });
-  ta.addEventListener('touchmove', function (e) {
-    var touchY = e.touches[e.touches.length - 1].clientY;
-    var touchX = e.touches[e.touches.length - 1].clientX;
-    if ((touchY - div.offsetTop) < div.offsetHeight * 0.9 || (touchX - div.offsetLeft) < div.offsetWidth * 0.9) {
-      e.stopPropagation();
-      // e.preventDefault();
-      div.style.left = (touchX - (((touchX - div.offsetLeft) > div.offsetWidth * 0.5) ? div.offsetWidth * 0.1 : div.offsetWidth * 0.9) - ta.offsetLeft) + 'px';
-      div.style.top = (touchY - (((touchY - div.offsetTop) > div.offsetHeight * 0.5) ? div.offsetHeight * 0.1 : div.offsetHeight * 0.9) - ta.offsetTop) + 'px';
-    }
-  });
       document.body.appendChild(div);
-    }
+    };
     // Handle Android menu entry click using nativewindow.js
     if (typeof self !== 'undefined' && self.port) {
       DEBUG_ADDON &&
